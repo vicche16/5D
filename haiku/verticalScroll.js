172670705b61eafac0555dc7c33e7a4eb050c545
vicche16 <47397621+vicche16@users.noreply.github.com>
2019-04-25 18:08:41 -0400

    Update verticalScroll.js

--- haiku/verticalScroll.js
+++ haiku/verticalScroll.js
@@ -1,19 +1,19 @@
 $("document").ready(function() {			
 		
 		$('.scroll2').click(function(){
-			$('.right').animate({
+			$('html, body').animate({
 				scrollTop: $('.container2').offset().top
 			}, 1000);				   				   
 		 }); 
 
 		$('.scroll3').click(function(){		   
-			$('.right').animate({
+			$('html, body').animate({
 				scrollTop: $('.container3').offset().top
 			}, 1000);	
 		 });							   	 
 			 
 		$('.scroll4').click(function(){			   
-		 	$('.right').animate({
+		 	$('html, body').animate({
 		 		scrollTop: $('.container4').offset().top
 		 	}, 1000);
 		 });
@@ -25,7 +25,7 @@ $("document").ready(function() {
 		 });*/	
 
 		$('.scroll5').click(function(){
-			$('.right').animate({
+			$('html, body').animate({
 				scrollTop: $('.container1').offset().top
 			}, 1000);				   
 		 });		
